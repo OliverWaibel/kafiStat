0bed5ffb1afa0e24dd60fe23e5e16075ede29298
Jonas Furrer <jonas@sequenz.net>
2013-09-20 16:04:32 +0200

    listens to post requests

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,47 +1,29 @@
-var sys = require("sys");   
-filesys = require("fs");  
 var express = require("express");
-var file= "../database/kafiStat.db";
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(file);
-var http = require('http');
+var sqlite3 = require("sqlite3");
 
-var app = express.createServer();
-app.get('/',function(req,res){
- res.send("Hello World");
+var file= "/home/d22/proj/kafiStat/database/kafiStat.db";
+
+var db = new sqlite3.Database(file);
+
+db.get("SELECT * FROM coffeestats", function(error, row) {
+    if(error && error.errno == 1) {
+        console.log("Table coffestats does not exist, creating");
+        db.run("CREATE TABLE coffeestats (INT PRIMARY KEY NOT NULL, timestamp timestamp NOT NULL, city string NOT NULL, street string NOT NULL, floor int NOT NULL, duration float NOT NULL);");
+    }
 });
 
-app.get('/kafiStat', function(req,res){
- db.query("select datum, maschine, stock from stat",function(records) {
-   res.write('<h1>Kaffee</h1>');
-   req.write('<table>\n');
-   for(var i = 0; i < records.length; i++)
-    {
-       res.write('<tr>\n');
-       res.write('<td>'+records[i].datum+'</td>\n');
-       res.write('<td>'+records[i].maschine+'</td>\n');
-       res.write('<td>'+records[i].stock+'</td>\n');
-       res.write('</tr>');
+var app = express();
+app.use(express.bodyParser());
 
-    }
-    res.write('</table>\n');
-    res.end();
-    });
+app.get('/',function(req,res){
+ res.send("THIS IS KAFISTATS!!");
+});
+
+app.post('/reportcoffee', function(request, response) {
+    response.send(request.body);
 });
 
 app.listen(8000);
 
-/**
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  stmt = db.prepare("INSERT INTO STAT values(strftime('%s','now'),1,2)");
-  stmt.run();
-  stmt.finalize();
-  response.end("Daten in DB gespeichert\n");
-});
-
-server.listen(8000);
-
-*/
 
 console.log("Server running at http://127.0.0.1:8000/");
